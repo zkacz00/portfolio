9034e4474e25d8dbfb877affe4a2a4c8ec94eacc
zkacz00 <zk.zofia.kaczmarczyk@gmail.com>
2025-06-16 10:46:49 +0200

    chchch

--- src/app/content/urls.ts
+++ src/app/content/urls.ts
@@ -1,5 +1,5 @@
-export const myCvPlUrl: string = "https://drive.google.com/file/d/112eeTTKKkLahc-iYufxF_YaFNSwu4ITs/view";
-export const myCvEnUrl: string = "https://drive.google.com/file/d/1whUZAJ1p-MzCWl72UhBuhtDC08UqT_yT/view";
+export const myCvPlUrl: string = "https://drive.google.com/file/d/1mj-1rxW0B0DrSQ109lPteefwz5tUqtMy/view";
+export const myCvEnUrl: string = "https://drive.google.com/file/d/1GwnjA2uILQe8V4veFV1tbWRqQB0c5kgK/view";
 export const myGithubUrl: string = "https://github.com/zkacz00";
 export const myLinkedinUrl: string = "https://www.linkedin.com/in/zofia-bronisz/";
 export const myLinkedinUrlShort: string = "linkedin.com/in/zofia-bronisz"
